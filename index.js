aa4eb04bd89e5c59d6de35b49e79451c7e96312b
Keshon Royster <k.royster512@gmail.com>
2022-12-13 09:15:36 -0600

    added function to get current weather however that data only displays in the console and doesn't render the cards

--- index.js
+++ index.js
@@ -9,3 +9,41 @@ const submitButton = () => {
     localStorage.setItem('cities' , JSON.stringify([cityName]));
 
 };
+
+//function to get weather data
+const getWeather = (city) => {
+    const getCurrentWeather  = 'https:api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey;
+    console.log(city);
+
+    //api call to get current weather
+    fetch(getCurrentWeather)
+        .then((res) => {
+            console.log(res);
+            return res.json();
+        })
+        .then((res) => {
+            console.log(res);
+            
+            const weather = document.querySelector('#current-Weather');
+
+            //weather card
+            const weatherCard = document.createElement('div').classList.add('border', 'border-ridge');
+            console.log(weatherCard);
+
+            //name of the city
+            const cityTitle =  document.createElement('h4').innerHTML = city + res.sys.country;
+            console.log
+
+            //current temp of the city
+            const currentTemperature = document.createElement('p').innerHTML = 'Current Temperature:' + res.main.temp + 'Farenheit';
+            //could not get degree symbol to populate (did alt 0176)
+
+            //current windspeed
+            const currentWindSpeed = document.createElement('p').innerHTML = 'Wind Speed:' + res.wind.speed + 'Mph';
+
+            //current humidity
+            const currentHumidity = document.createElement('p').innerHTML = res.main.humidity; + '%';
+            weatherCard.append(cityTitle, currentTemperature, currentWindSpeed, currentHumidity);
+            weather.append(weatherCard);
+        })
+};
