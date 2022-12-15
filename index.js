27984accef640e9fbf3a27135d4fafe2c3d5c09d
Keshon Royster <k.royster512@gmail.com>
2022-12-15 17:20:06 -0600

    created function to get forecast weather

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const getWeather = (city) => {
     //api call to get current weather
     fetch(getCurrentWeather)
         .then((res) => {
-            console.log(res);
+            // console.log(res);
             return res.json();
         })
         .then((res) => {
@@ -27,11 +27,10 @@ const getWeather = (city) => {
 
             //weather card
             const weatherCard = document.createElement('div').classList.add('border', 'border-ridge');
-            console.log(weatherCard);
+            // console.log(weatherCard);
 
             //name of the city
             const cityTitle =  document.createElement('h4').innerHTML = city + res.sys.country;
-            console.log
 
             //current temp of the city
             const currentTemperature = document.createElement('p').innerHTML = 'Current Temperature:' + res.main.temp + 'Farenheit';
@@ -45,14 +44,49 @@ const getWeather = (city) => {
             weatherCard.append(cityTitle, currentTemperature, currentWindSpeed, currentHumidity);
             weather.append(weatherCard);
         });
-    // const geoCordinates = 'https://api.openweathermap.org/geo/1.0/direct?q=' + city + '&appid=' + apiKey;
+    const geoCordinates = 'https://api.openweathermap.org/geo/1.0/direct?q=' + city + '&appid=' + apiKey;
     
     // api call for geo cordinates in order to get 5 day forecast
-    // fetch(geoCordinates)
-    // .then((res) => {
-    //     console.log(res);
-    //     return res.json();
-    // })
+    fetch(geoCordinates)
+    .then((res) => {
+        // console.log(res);
+        return res.json();
+    })
+    .then((res) => {
+        // console.log(res);
+        const latitude = res[0].lat;
+        const longitude = res[0].lon;
+        // console.log(latitude);
+        // console.log(longitude);
+        const forecastUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + latitude + '&lon=' + longitude + '&units=imperial&appid=' + apiKey;
+        fetch(forecastUrl)
+            .then((res) => {
+                // console.log(res);
+                return res.json();
+            })
+            .then((res) => {
+                console.log(res);
+                // function to make five day forecast card
+                const cityForecast = document.querySelector('#forecast');
+                cityForecast.remove();
+                 //weather card
+            const forecastWeatherCard = document.createElement('div').classList.add('border', 'border-ridge');
+            console.log(forecastWeatherCard);
+
+            const forecastTemperature = document.createElement('p').innerHTML = 'Current Temperature:' + res.main.temp + 'Farenheit';
+
+            const forecastMaxTemperature = document.createElement('p').innerHTML = 'Mininimu Temperature:' + res.main.temp + 'Farenheit';
+
+            const forecastMinTemperature = document.createElement('p').innerHTML = 'Max Temperature:' + res.main.temp + 'Farenheit';
+
+            const forecastWindSpeed = document.createElement('p').innerHTML = 'Wind Speed:' + res.wind.speed + 'Mph';
+
+            const forecastHumidity = document.createElement('p').innerHTML = res.main.humidity; + '%';
+
+            weatherCard.append(forecastTemperature, forecastMaxTemperature,forecastMinTemperature, forecastWindSpeed, forecastHumidity);
+            cityForecast.append(forecastWeatherCard);
+        });
+    });
 };
-// const onSubmit = document.querySelector('#btn');
-// onSubmit.addEventListener('click', submitButton);
+const onSubmit = document.querySelector('#btn');
+onSubmit.addEventListener('click', submitButton);
